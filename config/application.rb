ffdf2fcb077c9c17c12789e9498fc816a2a6a96d
Bryan Collazo <bcollazo@mit.edu>
2013-10-05 15:03:06 -0400

    Adding some files and some filepicker code

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module SyllabusEater
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+	config.filepicker_rails.api_key = "AiCW0KLWFQYinryH7oeOOz"
   end
 end
