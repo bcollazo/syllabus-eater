dfca272aea0b153821ed5810ad562d531055b413
Bryan Collazo <bcollazo@mit.edu>
2013-10-05 17:54:29 -0400

    Commiting to pull anthonys code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,19 +4,36 @@ class MainController < ApplicationController
 	end
 
 	def upload
+		ip_address = request.remote_ip
+
+
+		dir_string = "public/uploads/"+ip_address.to_s+"/"
+		puts dir_string
+		dir = File.dirname(dir_string)
+
+		puts dir
+
+		unless File.directory?(dir)
+			puts 'Making directory!'
+			FileUtils.mkdir_p(dir)
+		end
+
 		syllabi = ["1", "2", "3", "4", "5"]
 		syllabi.each do |s|
-			puts s
 			uploaded_io = params[s]
 			if uploaded_io == nil
 				next
 			end
-			File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-			file.write(uploaded_io.read)
+			File.open(Rails.root.join('public/uploads', ip_address, uploaded_io.original_filename), 'wb') do |file|
+				unless File.directory?(file)
+					puts 'Making directory!'
+					FileUtils.mkdir_p(dir)
+				end
+				file.write(uploaded_io.read)
 			end
 		end
 
-		value = %x(pwd 2>&1)
+		value = %x(cat 'public/upload/18510.pdf' 2>&1)
 		puts "BRYAN ESSS EL CABALLO!! ", value
 
 		redirect_to dashboard_url
