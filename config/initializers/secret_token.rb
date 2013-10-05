5d253f3bbf08225b6cbdf43a14ad413df82c3e8b
Bryan Collazo <bcollazo@mit.edu>
2013-10-05 13:40:10 -0400

    New empty rails project.  Deleted test txt files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SyllabusEater::Application.config.secret_token = 'c0f36846b8d34dec79d0368ccf08943c8f6b93d071bcbe08b10e7c1bd4e1758268abdb244095f3dba59fa060b1cb882c11c13f0ed9057d68044b3b932e1410e3'
