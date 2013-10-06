a9cb2eecbe7f3021367fb3d24f0fa8d09a4b413a
tonypr <tonypr100@gmail.com>
2013-10-05 23:14:03 -0400

    Revert "updating"
    This reverts commit f0135b8b1da6c395b05384e0f0bc41d2f4b9034b.

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'd9a4b2446e193178da0138d2d2c6a62ec447069b9e017770b1aee2be97a3e6d279ccfb5c79f860d8a848510b193c57626c9ea418bcfb86f3401f507c2cf63f83';
--- h/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '046df5970deb2f0b24bab4b960a1ad854291775e3e13249c8db5946d45824604c7a3d22870c8651fd2e87b16a29832ae19168b96c183db448af0ec5c42e6c6de';
